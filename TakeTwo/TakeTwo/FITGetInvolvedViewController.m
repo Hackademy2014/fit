694026dd794daf1f70a95c3a9ae0b5c153d2f16d
Anthony Urena <agu1@wildcats.unh.edu>
2014-05-17 14:51:59 -0400

    added empty ViewControllers

---
+++ TakeTwo/TakeTwo/FITGetInvolvedViewController.m
@@ -0,0 +1,49 @@
+//
+//  FITGetInvolvedViewController.m
+//  TakeTwo
+//
+//  Created by Anthony Urena on 5/17/14.
+//  Copyright (c) 2014 Families In Transition. All rights reserved.
+//
+
+#import "FITGetInvolvedViewController.h"
+
+@interface FITGetInvolvedViewController ()
+
+@end
+
+@implementation FITGetInvolvedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
